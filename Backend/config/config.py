06a36f075ff46caceb556d734c5f1e83ba5ff25d
Suhas Magadum <suhasmagadum05@gmail.com>
2026-02-15 01:47:00 +0530

    remove duplicate ssl config for neon

--- Backend/config/config.py
+++ Backend/config/config.py
@@ -7,9 +7,3 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        "connect_args": {
-            "sslmode": "require"
-        }
-    }
